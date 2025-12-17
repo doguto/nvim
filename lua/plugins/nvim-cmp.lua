c50a473a9fc564673419c7900bde0443e0fbdafa
tougiTakahiro <tougi.takahiro@grimoire.co>
2025-12-17 10:10:25 +0900

    initial commit

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,36 @@
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		{"hrsh7th/cmp-buffer"},
+		{"saadparwaiz1/cmp_luasnip"},
+		{"L3MON4D3/LuaSnip"},
+		{"rafamadriz/friendly-snippets"},
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		require("luasnip/loaders/from_vscode").lazy_load()
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<c-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "luasnip", priority_weight = 20 },
+
+			}, {
+				{ name = "buffer", priority_weight = 10 },
+			}),
+		})
+	end,
+}
