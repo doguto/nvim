3a6af0cd6ba97c390522e8230ae5269afbcf8547
doguto <doguto110@gmail.com>
2026-05-15 14:57:07 +0900

    Feat: CSVの作成

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -26,6 +26,7 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/site")
 vim.diagnostic.config({
   underline = true,
   virtual_text = false,
